df8479a4cba2f3aa6ceb7cc3156e006afa91679e
Vroon123 <varunpanthri38@gmail.com>
2018-04-17 14:51:22 +0530

    Lat long parsed

--- sketch_apr16b.ino
+++ sketch_apr16b.ino
@@ -1,59 +1,81 @@
 int numarray[6] = { -1, -1, -1, -1, -1, -1}; //checksum -(36 71 78 82 77 67) = -411
 int i = 0;
-String response = ""; 
+String response = "";
 String toprint = "";
 int comma[10];
+byte gps;
+int  sum = 0;
+
+String current_lat;
+String current_long;
+String current_alt ;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial2.begin(9600);
-}
 
-void loop() {
-  int  sum = 0;
   while (Serial2.available() > 0) {
-    byte gps = Serial2.read();
-    
-    if (numarray[5] < 0)
+    gps = Serial2.read();
+
+    while (numarray[5] < 0)
     {
-      
+
       numarray[i] = int(gps);
       char a = numarray[i];
-      toprint = toprint+a;
+      toprint = toprint + a;
       Serial.print(numarray[i]);
       i++;
     }
-    else
-    {
-      shift(int(gps));
-      sum = numarray[0] + numarray[1] + numarray[2] + numarray[3] + numarray[4] + numarray[5] - 411;
-      if (sum == 0) {
-      Serial.print(toprint);
-       while(1){
-       byte data = Serial2.read();
-       char d = int(data);
-       Serial.print(d);
-       response += d; 
-       if(d=='$')
-       break;
-        
-      } 
-       // Serial.println(response);
-       comma[0] = response.indexOf(',');
+  }
+
+}
+
+void loop() {
+  
+if (Serial2.available() > 0) {
+  gps = Serial2.read();
+  shift(int(gps));
+  sum = numarray[0] + numarray[1] + numarray[2] + numarray[3] + numarray[4] + numarray[5] - 411;
+  if (sum == 0) {
+    response = "";
+   // Serial.print("toprint");
+    while (1) {
+      if (Serial2.available() > 0) {
+        byte data = Serial2.read();
+        char d = int(data);
+       // Serial.print(d);
+        response += d;
+        if (d == '$')
+          { 
+            break;
+          }
+          
+          }
+    }
+    //Serial.println(response);
+    //Serial.println(response.length());
+    if(response.length()>=55)
+      {
+        comma[0] = response.indexOf(',');
         for(int i=1;i<6;i++){
              comma[i] = response.indexOf(',', comma[i-1] + 1 );
          }
       delay(100);
       current_lat = response.substring(comma[2]+1 ,comma[3]);
-      current_long = response.substring(comma[3]+1,comma[4]);
-      current_alt = response.substring(comma[4]+1,comma[5]);
-        }
+      current_long = response.substring(comma[4]+1,comma[5]);
+     
+       Serial.print("Latitude : ");
+       Serial.println(current_lat);
+       Serial.print("Longitude : ");
+       Serial.println(current_long);
+       
       }
-    }
+  }
+}
 }
 
-  //Serial.print(numarray);
+//Serial.print(numarray);
 
 
 void shift(int byt) {
@@ -62,7 +84,7 @@ void shift(int byt) {
     numarray[j] = numarray[j + 1];
   }
   char a = byt;
-  toprint = toprint.substring(1)+ a ;
-  
+  toprint = toprint.substring(1) + a ;
+
   numarray[5] = byt;
 }
